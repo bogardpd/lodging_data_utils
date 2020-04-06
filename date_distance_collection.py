342abbaedbe65cbabcaa13c703745639818791b1
Paul Bogard <paulbogard@gmail.com>
2020-04-05 21:23:13 -0400

    Write DateDistanceCollection.set_distance

--- date_distance_collection.py
+++ date_distance_collection.py
@@ -1,20 +1,38 @@
-import datetime
+from datetime import date, timedelta
 from dateutil import rrule
+
 
 class DateDistanceCollection:
     """Manages a range of dates with a distance assigned to each."""
 
-    def __init__(self, start_year, end_year):
-        """Initialize a DateDistances instance."""
-        start_date = datetime.date(start_year, 1, 1)
-        end_date = datetime.date(end_year, 12, 31)
+    def __init__(self, start_year, end_year, home_location):
+        """Initialize a DateDistanceCollection."""
+        self.home = home_location
+        # TODO: look up home coordinates here so we don't have to look
+        # them up every time.
 
-        self.distances = {
-            d.date():0 for d in list(
-                rrule.rrule(
-                    rrule.DAILY,
-                    dtstart=start_date,
-                    until=end_date))}
+        # Create dictionary of dates with distance set to 0 by default:
+        start_date = date(start_year, 1, 1)
+        end_date = date(end_year, 12, 31)
+        self.distances = {d:0 for d in self.__inclusive_date_range(
+            start_date, end_date)}
         
-    def set_distance(self, checkout_date, nights, distance):
+    def __home_distance(self, location):
+        """Returns the distance from home to the provided location."""
+        return 1000 # PLACEHOLDER
+
+    def __inclusive_date_range(self, start_date, end_date):
+        """Returns a list of date objects in the given range."""
+        return([d.date() for d in list(rrule.rrule(rrule.DAILY,
+            dtstart=start_date, until=end_date))])
+
+    def set_distance(self, checkout_date, nights, location):
         """ Sets the distances for the dates in a given hotel stay."""
+        dates = self.__inclusive_date_range(
+            checkout_date - timedelta(days=(nights-1)),
+            checkout_date)
+        for day in dates:
+            if day in self.distances.keys():
+                self.distances[day] = self.__home_distance(location)
+    
+    
--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import pandas as pd
+from datetime import date
 from pathlib import Path
 from date_distance_collection import DateDistanceCollection
 
 HOTEL_FILE_PATH = Path("C:/Users/paulb/OneDrive/Documents/Travel/") / "Hotels.xlsx"
+HOME_LOCATION = "Beavercreek, OH"
 
-distances = DateDistanceCollection(2009, 2020)
+distances = DateDistanceCollection(2020, 2020, HOME_LOCATION)
+
+distances.set_distance(date(2020,12,25), 5, "Chicago, IL")
+
 
 for k, v in distances.distances.items():
     print(k, v)
 
-hotel_data_sheet = pd.read_excel(HOTEL_FILE_PATH, sheet_name='Hotel Data')
-hotel_data = hotel_data_sheet[['City', 'Checkout Date', 'Nights']]
+# hotel_data_sheet = pd.read_excel(HOTEL_FILE_PATH, sheet_name='Hotel Data')
+# hotel_data = hotel_data_sheet[['City', 'Checkout Date', 'Nights']]
 
 # for row in hotel_data.values.tolist():
 #     print(row)
