b1df4d965e9268debb86d1243e0caf8dbefd23e7
Paul Bogard <paulbogard@gmail.com>
2020-04-07 23:32:51 -0400

    Fix line breaks in heatmap_data

--- heatmap_data.py
+++ heatmap_data.py
@@ -3,14 +3,17 @@ from datetime import date
 from pathlib import Path
 from modules.date_collection import DateCollection
 
-HOTEL_FILE_PATH = Path("C:/Users/paulb/OneDrive/Documents/Travel/") / "Hotels.xlsx"
+HOTEL_FILE_PATH = (Path("C:/Users/paulb/OneDrive/Documents/Travel/")
+    / "Hotels.xlsx")
 OUTPUT_FILE_PATH = "output/heatmap_data.csv"
 
-hotel_data_sheet = pd.read_excel(HOTEL_FILE_PATH, sheet_name='Hotel Data')
+hotel_data_sheet = pd.read_excel(
+    HOTEL_FILE_PATH, sheet_name='Hotel Data')
 hotel_data = hotel_data_sheet[['Checkout Date', 'Nights', 'City']]
 
 earliest_hotel_id = hotel_data['Checkout Date'].idxmin()
-earliest_checkout = hotel_data.loc[earliest_hotel_id,'Checkout Date'].date()
+earliest_checkout = hotel_data.loc[
+    earliest_hotel_id,'Checkout Date'].date()
 earliest_nights = int(hotel_data.loc[earliest_hotel_id,'Nights'])
 
 min_date = DateCollection.first_morning(
@@ -26,5 +29,7 @@ with open(OUTPUT_FILE_PATH, 'w') as f:
     f.write("Date,Latitude,Longitude\n")
     for date, location in locations.locations.items():
         if location != None:
-            f.write(f"{date},{location['coordinates'][0]},{location['coordinates'][1]}\n")
+            f.write(f"{date},"
+            f"{location['coordinates'][0]},"
+            f"{location['coordinates'][1]}\n")
     print(f"Wrote to {OUTPUT_FILE_PATH}")
