606c406a5d20642cc1f0ba359ee26a5ea7dd341b
Paul Bogard <paulbogard@gmail.com>
2020-04-09 19:06:47 -0400

    Make DateDistanceCollection a child class of DateCollection

--- distance_from_home_by_day.py
+++ distance_from_home_by_day.py
@@ -1,16 +1,16 @@
-from modules.date_distance_collection import DateDistanceCollection
+from modules.date_collection import DateDistanceCollection
 from modules.hotel_data_frame import HotelDataFrame
 
 OUTPUT_FILE_PATH = "output/distance_from_home_by_day.txt"
 HOME_LOCATION = "US/OH/Beavercreek"
 
 hotel_df = HotelDataFrame()
-distances = DateDistanceCollection(2009, 2020, HOME_LOCATION)
+locations = DateDistanceCollection(2009, 2020, HOME_LOCATION)
 
 for row in hotel_df.data.values.tolist():
-    distances.set_distance(*row)
+    locations.set_location(*row)
 
 with open(OUTPUT_FILE_PATH, 'w') as f:
-    for k, v in distances.distances.items():
+    for k, v in locations.locations.items():
         f.write (f"{k}\t{v}\n")
     print(f"Wrote to {OUTPUT_FILE_PATH}")
--- modules/date_collection.py
+++ modules/date_collection.py
@@ -13,9 +13,7 @@ class DateCollection:
     """Manages a range of dates with a location assigned to each."""
     
     COORDINATES_PATH = "data/coordinates.json"
-    EARTH_MEAN_RADIUS = 3958.7613 # miles
-    DEG_TO_RAD = math.pi / 180
-
+    
     def __init__(self, start_date, end_date, default_location=None):
         """Initialize a DateCollection."""
         
@@ -24,12 +22,12 @@ class DateCollection:
         
         default_value = None if default_location == None else {
             'city': default_location,
-            'coordinates': self.__coordinates(default_location)}
+            'coordinates': self._coordinates(default_location)}
         # Create dictionary of dates with locations set to None by default:
-        self.locations = {d:default_value for d in self.__inclusive_date_range(
+        self.locations = {d:default_value for d in self._inclusive_date_range(
             start_date, end_date)}
 
-    def __coordinates(self, location):
+    def _coordinates(self, location):
         """Returns the coordinates for a location."""
         location_list = location.split("/")
         try:
@@ -41,14 +39,14 @@ class DateCollection:
             print(f"\nPlease add it to {self.COORDINATES_PATH}.\n")
             raise SystemExit()
 
-    def __inclusive_date_range(self, start_date, end_date):
+    def _inclusive_date_range(self, start_date, end_date):
         """Returns a list of date objects in the given range."""
         return([d.date() for d in list(rrule.rrule(rrule.DAILY,
             dtstart=start_date, until=end_date))])
 
     def set_location(self, checkout_date, nights, location):
-        """ Sets the distances for the dates in a given hotel stay."""
-        dates = self.__inclusive_date_range(
+        """ Sets the locations for the dates in a given hotel stay."""
+        dates = self._inclusive_date_range(
             first_morning(checkout_date, nights),
             checkout_date)
         
@@ -56,6 +54,66 @@ class DateCollection:
             if day in self.locations.keys():
                 self.locations[day] = {
                     'city': location,
-                    'coordinates': self.__coordinates(location)}
+                    'coordinates': self._coordinates(location)}
+
+
+class DateDistanceCollection(DateCollection):
+    """Manages a range of dates with a distance assigned to each."""
     
-    
+    EARTH_MEAN_RADIUS = 3958.7613 # miles
+    DEG_TO_RAD = math.pi / 180
+
+    def __init__(self, start_year, end_year, home_location):
+        """Initialize a DateDistanceCollection."""
+        
+        start_date = date(start_year, 1, 1)
+        end_date = date(end_year, 12, 31)
+
+        super().__init__(start_date, end_date, home_location)
+        self.home_coordinates = self._coordinates(home_location)
+
+        # Add distances to locations dictionary:
+        for day, loc in self.locations.items():
+            self.locations[day] = {**loc, 'distance': 0.0}
+            
+   
+    def _home_distance(self, location):
+        """Returns the distance from home to the provided location.
+        
+        The Haversine formula is used to calculate the distance.
+        """
+        location_coordinates = self._coordinates(location)
+        phi_1 = self.home_coordinates[0] * self.DEG_TO_RAD
+        phi_2 = location_coordinates[0] * self.DEG_TO_RAD
+        delta_phi = (
+            (location_coordinates[0] - self.home_coordinates[0])
+            * self.DEG_TO_RAD)
+        delta_lambda = (
+            (location_coordinates[1] - self.home_coordinates[1])
+            * self.DEG_TO_RAD)
+        a = (math.sin(delta_phi/2)**2
+            + math.cos(phi_1)
+            * math.cos(phi_2)
+            * math.sin(delta_lambda/2)**2)
+        distance = (2
+            * self.EARTH_MEAN_RADIUS
+            * math.atan2(math.sqrt(a), math.sqrt(1-a)))
+        return distance
+
+    def set_location(self, checkout_date, nights, location):
+        """ Sets locations and distances.
+        
+        Sets the location, coordinates, and distances for the dates in a
+        given hotel stay.
+        """
+        dates = self._inclusive_date_range(
+            first_morning(checkout_date, nights),
+            checkout_date)
+        distance = self._home_distance(location)
+        
+        for day in dates:
+            if day in self.locations.keys():
+                self.locations[day] = {
+                    'city': location,
+                    'coordinates': self._coordinates(location),
+                    'distance': distance}
