600c1b9d1d0add17f36ac74038e0c92a04101d2c
Paul Bogard <paulbogard@gmail.com>
2020-04-12 19:49:44 -0400

    Write group_stays method for nights_away_and_home.py

--- nights_away_and_home.py
+++ nights_away_and_home.py
@@ -1,50 +1,89 @@
 import yaml
-from modules.common import checkin_date
-from modules.hotel_data_frame import HotelDataFrame
 from datetime import date
 
+from modules.common import checkin_date
+from modules.hotel_data_frame import HotelDataFrame
+
 OUTPUT_FILE_PATH = 'output/nights_away_and_home.yaml'
+START_DATE = date(2009,2,10)
+END_DATE = date.today()
+
+def group_stays(hotel_data_frame):
+    """Groups consecutive away-from-home stays."""
+    grouped = []
+    previous_checkout = None
+    for stay in hotel_df.data.values.tolist():
+        checkout = stay[0].date()
+        nights = stay[1]
+        city = stay[2]
+        checkin = checkin_date(checkout, nights)
+        
+        if (len(grouped) == 0) or checkin != previous_checkout:
+            # Create new group:
+            
+            grouped.append({
+                'start': checkin,
+                'end': checkout,
+                'nights': (checkout - checkin).days,
+                'cities': [{
+                    'city': city,
+                    'start': checkin,
+                    'end': checkout,
+                    'nights': (checkout - checkin).days
+                    }]
+                })
+                
+        else:
+            # Merge into previous group:
+            grouped[-1]['end'] = checkout
+            grouped[-1]['nights'] = (checkout - grouped[-1]['start']).days
+            
+            if len(grouped[-1]['cities']) == 0 or grouped[-1]['cities'][-1]['city'] != city:
+                # Create a new city:
+                grouped[-1]['cities'].append({
+                    'city': city,
+                    'start': checkin,
+                    'end': checkout,
+                    'nights': (checkout - checkin).days
+                })
+            else:
+                # Merge into previous city:
+                grouped[-1]['cities'][-1]['end'] = checkout
+
+        previous_checkout = checkout
+    return grouped
+
+def create_rows(grouped_stays):
+    """Creates a row for each away period/home period pair.
+
+    Calculates the length of each home stay between two away groups.
+    """
+    away_home_rows = []
+    for i, group in enumerate(grouped_stays):
+        home_start = group['end']
+        if i == len(grouped_stays) - 1:
+            home_end = END_DATE 
+        else:
+            home_end = grouped_stays[i + 1]['start']
+        away_nights = (group['end'] - group['start']).days
+        away = group
+        home = {
+            'start': home_start,
+            'end': home_end,
+            'nights': (home_end - home_start).days
+        }
+        away_home_rows.append({'away': away, 'home': home})
+    return(away_home_rows)
+
+
+# MAIN SCRIPT:
 
 hotel_df = HotelDataFrame()
+grouped = group_stays(hotel_df)
+# TODO: Filter by start and end date
+away_home_rows = create_rows(grouped)
 
-grouped = []
-previous_checkout = None
-for stay in hotel_df.data.values.tolist():
-    checkout = stay[0].date()
-    nights = stay[1]
-    city = stay[2]
-    checkin = checkin_date(checkout, nights)
-    
-    if (len(grouped) == 0) or checkin != previous_checkout:
-        # Create new group:
-        
-        grouped.append({
-            'start': checkin,
-            'end': checkout,
-            'cities': [{
-                'start': checkin,
-                'end': checkout,
-                'city': city
-                }]
-            })
-            
-    else:
-        # Merge into previous group:
-        
-        if len(grouped[-1]['cities']) == 0 or grouped[-1]['cities'][-1]['city'] != city:
-            # Create a new city:
-            grouped[-1]['cities'].append({
-                'start': checkin,
-                'end': checkout,
-                'city': city
-            })
-        else:
-            # Merge into previous city:
-            grouped[-1]['cities'][-1]['end'] = checkout
-
-        grouped[-1]['end'] = checkout
-    
-    previous_checkout = checkout
-
+# Write to file:
 with open(OUTPUT_FILE_PATH, 'w', encoding="utf-8") as f:
-    yaml.dump(grouped, f, allow_unicode=True, sort_keys=False)
+    yaml.Dumper.ignore_aliases = lambda *args : True # Avoid references
+    yaml.dump(away_home_rows, f, allow_unicode=True, sort_keys=False)
