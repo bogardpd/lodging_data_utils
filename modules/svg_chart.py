3fde5fb1b14b424e87455868938b5bdb99675e27
Paul Bogard <paulbogard@gmail.com>
2020-04-16 22:24:21 -0400

    Add header to SVGChart

--- modules/svg_chart.py
+++ modules/svg_chart.py
@@ -8,12 +8,16 @@ class SVGChart:
 
     _NSMAP = {None: "http://www.w3.org/2000/svg"}
     _PARAMS = {
+        'header': {
+            'height': 40, # px
+            'text_offset': [6, 25] # [x, y] px
+        },
         'night': {
             'cell_size': 8, # px
-            'radius': 3, # px
+            'radius': 3 # px
         },
         'page': {
-            'margin': 40, # px
+            'margin': 40 # px
         },
         'year': {
             'margin': 50, # px
@@ -46,29 +50,79 @@ class SVGChart:
             + PARAMS['year']['margin'])
         values['dims']['home_width'] = ((1.5 + self.home_max)
             * PARAMS['night']['cell_size'])
+        values['dims']['chart_width'] = (values['dims']['away_width']
+            + values['dims']['home_width'])
         values['dims']['chart_height'] = (
             (len(self.stays) + 2) * PARAMS['night']['cell_size'])
         values['dims']['page_width'] = (double_margin
-            + values['dims']['away_width'] + values['dims']['home_width'])
+            + values['dims']['chart_width'])
         values['dims']['page_height'] = (double_margin
+            + PARAMS['header']['height']
             + values['dims']['chart_height'])
         
         values['coords']['axis_anchor'] = [
             PARAMS['page']['margin'] + values['dims']['away_width'],
             PARAMS['page']['margin']]
-        values['coords']['chart_top_left'] = [
-            PARAMS['page']['margin'],
-            PARAMS['page']['margin']]
-        values['coords']['chart_bottom_right'] = [
-            (PARAMS['page']['margin']
-                + values['dims']['away_width'] + values['dims']['home_width']),
-            PARAMS['page']['margin'] + values['dims']['chart_height']]
         values['coords']['night_anchor'] = [
             values['coords']['axis_anchor'][0],
             (values['coords']['axis_anchor'][1]
+                + PARAMS['header']['height']
                 + (1.5 * PARAMS['night']['cell_size']))]
+        values['coords']['header'] = {
+            'l': PARAMS['page']['margin'],
+            'r': PARAMS['page']['margin'] + values['dims']['chart_width'],
+            't': PARAMS['page']['margin'],
+            'b': PARAMS['page']['margin'] + PARAMS['header']['height']
+        }
+        values['coords']['chart'] = {
+            'l': PARAMS['page']['margin'],
+            'r': PARAMS['page']['margin'] + values['dims']['chart_width'],
+            't': values['coords']['header']['b'],
+            'b': (values['coords']['header']['b']
+                + values['dims']['chart_height'])
+        }
 
         return(values)
+
+    def _draw_backgrounds(self):
+        """Draws background shading."""
+
+        COORDS = self._vals['coords']
+        DIMS = self._vals['dims']
+        PARAMS = self._PARAMS
+
+        g_years = xml.SubElement(self._root, "g", id="year_background")
+        
+        # Determine [x, y] coordinates of each Jan 1 circle:
+        year_starts = {}
+        for row_index, row in enumerate(self.stays):
+            for stay_loc in ['away', 'home']:
+                if row[stay_loc]['start'].year < row[stay_loc]['end'].year:
+                    # This stay contains a night ending on 1 January
+                    mornings = stay_mornings(
+                        row[stay_loc]['start'], row[stay_loc]['end'])
+                    night_index = next(i for i, v in enumerate(mornings) if (
+                        v.month == 1 and v.day == 1))
+                    away_nights = (row[stay_loc]['nights'] if stay_loc == 'away'
+                        else None)
+
+                    year_starts[mornings[night_index].year] = (
+                        self._night_center(
+                            row_index, night_index, away_nights))
+        
+        years = sorted(year_starts.keys())
+        if len(years) == 0:
+            self._draw_year_background(g_years, None, None, None, 1)
+        else:
+            self._draw_year_background(
+                g_years, years[0] - 1, None, year_starts.get(years[0]), 1)
+            for i, year in enumerate(years):
+                self._draw_year_background(
+                    g_years,
+                    year,
+                    year_starts.get(year),
+                    year_starts.get(year + 1),
+                    i % 2)
 
     def _draw_gridlines(self):
         """Draws vertical gridlines.
@@ -82,8 +136,8 @@ class SVGChart:
 
         week_x = list(range(int(-self.away_max/7), int(self.home_max/7) + 1))
         
-        top = self._vals['coords']['chart_top_left'][1]
-        bottom = self._vals['coords']['chart_bottom_right'][1]
+        top = self._vals['coords']['chart']['t']
+        bottom = self._vals['coords']['chart']['b']
 
         for week in week_x:
             style_class = 'axis' if week == 0 else 'gridline'
@@ -91,6 +145,52 @@ class SVGChart:
                 + (PARAMS['night']['cell_size'] * 7 * week))
             xml.SubElement(g_weeks, "line", x1 = str(x), y1 = str(top),
                 x2 = str(x), y2 = str(bottom)).set('class', style_class)
+
+    def _draw_header(self):
+        """Draws a header."""
+
+        axis_anchor = self._vals['coords']['axis_anchor']
+        offset = self._PARAMS['header']['text_offset']
+        bounds = self._vals['coords']['header']
+
+        g_header = xml.SubElement(self._root, "g", id="header")
+
+        xml.SubElement(g_header, "rect",
+            x = str(bounds['l']),
+            y = str(bounds['t']),
+            width=str(self._vals['dims']['chart_width']),
+            height=str(self._PARAMS['header']['height'])).set(
+                'class', "header")
+
+        xml.SubElement(g_header, "line",
+            x1 = str(bounds['l']),
+            y1 = str(bounds['b']),
+            x2 = str(bounds['r']),
+            y2 = str(bounds['b'])).set(
+                'class', "axis")
+
+        header_away = xml.SubElement(g_header, "text",
+            x=str(axis_anchor[0] - offset[0]),
+            y=str(axis_anchor[1] + offset[1]))
+        header_away.set('class', "header header-away")
+        header_away.text = "Nights on "
+        header_away_business = xml.SubElement(header_away, "tspan")
+        header_away_business.set('class', "header-sub night-away-business")
+        header_away_business.text = "work"
+        header_away_business.tail = "/"
+        header_away_personal = xml.SubElement(header_away, "tspan")
+        header_away_personal.set('class', "header-sub night-away-personal")
+        header_away_personal.text = "personal"
+        header_away_personal.tail = " trips"
+
+        header_home = xml.SubElement(g_header, "text",
+            x=str(axis_anchor[0] + offset[0]),
+            y=str(axis_anchor[1] + offset[1]))
+        header_home.set('class', "header header-home")
+        header_home.text = "Nights at "
+        header_home_home = xml.SubElement(header_home, "tspan")
+        header_home_home.set('class', "header-sub night-home")
+        header_home_home.text = "home"
 
     def _draw_nights(self):
         """Draws a dot for each night."""
@@ -127,38 +227,37 @@ class SVGChart:
                               start_coord, end_coord, fill_index):
         """Draws background shading for a specific year."""
         PARAMS = self._PARAMS
-        left = self._vals['coords']['chart_top_left'][0]
-        top = self._vals['coords']['chart_top_left'][1]
-        right = self._vals['coords']['chart_bottom_right'][0]
-        bottom = self._vals['coords']['chart_bottom_right'][1]
+        bounds = self._vals['coords']['chart']
         half_cell = PARAMS['night']['cell_size'] / 2
-
+        
         poly_coords = []
+        
         # Create top points, left to right:
         if start_coord == None:
             # First year
-            poly_coords.append([left, top])
-            poly_coords.append([right, top])
+            poly_coords.append([bounds['l'], bounds['t']])
+            poly_coords.append([bounds['r'], bounds['t']])
+            
         else:
             start_top = start_coord[1] - half_cell
             start_bottom = start_coord[1] + half_cell
-            poly_coords.append([left, start_bottom])
+            poly_coords.append([bounds['l'], start_bottom])
             poly_coords.append([start_coord[0], start_bottom])
             poly_coords.append([start_coord[0], start_top])
-            poly_coords.append([right, start_top])
+            poly_coords.append([bounds['r'], start_top])
 
         # Create bottom points, right to left:
         if end_coord == None:
             # Final year
-            poly_coords.append([right, bottom])
-            poly_coords.append([left, bottom])
+            poly_coords.append([bounds['r'], bounds['b']])
+            poly_coords.append([bounds['l'], bounds['b']])
         else:
             end_top = end_coord[1] - half_cell
             end_bottom = end_coord[1] + half_cell
-            poly_coords.append([right, end_top])
+            poly_coords.append([bounds['r'], end_top])
             poly_coords.append([end_coord[0], end_top])
             poly_coords.append([end_coord[0], end_bottom])
-            poly_coords.append([left, end_bottom])
+            poly_coords.append([bounds['l'], end_bottom])
 
         poly_points_str = " ".join(
             list(",".join(
@@ -173,46 +272,6 @@ class SVGChart:
                 y=str(poly_coords[0][1] + PARAMS['year']['text_offset'][1]))
             year_text.set('class', "year-label")
             year_text.text = str(year)
-
-    def _draw_year_backgrounds(self):
-        """Draws background shading for all years."""
-
-        COORDS = self._vals['coords']
-        DIMS = self._vals['dims']
-        PARAMS = self._PARAMS
-
-        g_years = xml.SubElement(self._root, "g", id="year_background")
-        
-        # Determine [x, y] coordinates of each Jan 1 circle:
-        year_starts = {}
-        for row_index, row in enumerate(self.stays):
-            for stay_loc in ['away', 'home']:
-                if row[stay_loc]['start'].year < row[stay_loc]['end'].year:
-                    # This stay contains a night ending on 1 January
-                    mornings = stay_mornings(
-                        row[stay_loc]['start'], row[stay_loc]['end'])
-                    night_index = next(i for i, v in enumerate(mornings) if (
-                        v.month == 1 and v.day == 1))
-                    away_nights = (row[stay_loc]['nights'] if stay_loc == 'away'
-                        else None)
-
-                    year_starts[mornings[night_index].year] = (
-                        self._night_center(
-                            row_index, night_index, away_nights))
-        
-        years = sorted(year_starts.keys())
-        if len(years) == 0:
-            self._draw_year_background(g_years, None, None, None, 0)
-        else:
-            self._draw_year_background(
-                g_years, years[0] - 1, None, year_starts.get(years[0]), 0)
-            for i, year in enumerate(years):
-                self._draw_year_background(
-                    g_years,
-                    year,
-                    year_starts.get(year),
-                    year_starts.get(year + 1),
-                    (i + 1) % 2)
 
     def _filter_dates(self, grouped_stay_rows, start_date=None, end_date=None):
         """Filters group stay rows by date."""
@@ -246,8 +305,9 @@ class SVGChart:
         """Generates an SVG chart based on the away/home row values."""
         
         self._import_styles()
-        self._draw_year_backgrounds()
+        self._draw_backgrounds()
         self._draw_gridlines()
+        self._draw_header()
         self._draw_nights()
                         
         tree = xml.ElementTree(self._root)
