81e50d8f39f20ac027a48bbe25625ffc01edff35
Paul Bogard <paulbogard@gmail.com>
2020-04-26 15:16:11 -0400

    Add city to heatmap CSV output

--- heatmap_data.py
+++ heatmap_data.py
@@ -11,10 +11,11 @@ locations = DateCollection(hotel_df, min_date, date.today())
 # locations = DateCollection(min_date, date.today(), "US/OH/Beavercreek")
 
 with open(OUTPUT_FILE_PATH, 'w') as f:
-    f.write("Date,Latitude,Longitude\n")
+    f.write("Date,Latitude,Longitude,City\n")
     for date, location in locations.locations.items():
         if location != None:
             f.write(f"{date},"
             f"{location['coordinates'][0]},"
-            f"{location['coordinates'][1]}\n")
+            f"{location['coordinates'][1]},"
+            f"{location['city']}\n")
     print(f"Wrote to {OUTPUT_FILE_PATH}")
