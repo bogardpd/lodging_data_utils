159209ebb8306ada6fe5d31b2ac22ce26b59e8e6
Paul Bogard <paulbogard@gmail.com>
2020-07-21 13:28:42 -0400

    Add date filter options to HotelDataFrame.location_frequencies

--- location_frequency_map_data.py
+++ location_frequency_map_data.py
@@ -3,20 +3,37 @@ from pathlib import Path
 from modules.collections import DateCollection
 from modules.hotel_data_frame import HotelDataFrame
 
-OUTPUT_FILE_PATH = (Path.home()
-        / Path("OneDrive/Projects/Maps/Hotel Nights/Data")
-        / Path("Location Frequencies.csv"))
+OUTPUT_DIR = Path.home() / Path("OneDrive/Projects/Maps/Hotel Nights/Data")
 
-hotel_df = HotelDataFrame()
-loc_freqs = hotel_df.location_frequencies()
+def main():
+    hotel_df = HotelDataFrame()
 
-with open(OUTPUT_FILE_PATH, 'w') as f:
-    f.write("City,Latitude,Longitude,Frequency")
-    for city, details in sorted(loc_freqs.items()):
-        f.write(
-            f"\n{city},"
-            f"{details['latitude']},"
-            f"{details['longitude']},"
-            f"{details['frequency']}"
+    # Create CSV for all data.
+    loc_freqs = hotel_df.location_frequencies()
+    write_csv("All Dates.csv", loc_freqs)
+
+    # Create CSV for each year.
+    earliest_year = hotel_df.min_date().year
+    latest_year = date.today().year
+    for year in range(earliest_year, latest_year+1):
+        year_loc_freqs = hotel_df.location_frequencies(
+            start_date=date(year, 1, 1),
+            end_date=date(year, 12, 31)
         )
-    print(f"Wrote to {OUTPUT_FILE_PATH}")
+        write_csv(f"{year}.csv", year_loc_freqs)
+
+def write_csv(filename, data):
+    filepath = OUTPUT_DIR / Path(filename)
+    with open(filepath, 'w', encoding='UTF-8', newline='') as f:
+        f.write("City,Latitude,Longitude,Frequency")
+        for city, details in sorted(data.items()):
+            f.write(
+                f"\n{city},"
+                f"{details['latitude']},"
+                f"{details['longitude']},"
+                f"{details['frequency']}"
+            )
+        print(f"Wrote to {filepath}")
+
+if __name__ == "__main__":
+    main()
--- modules/hotel_data_frame.py
+++ modules/hotel_data_frame.py
@@ -31,7 +31,10 @@ class HotelDataFrame:
         
         return(first_morning(earliest_checkout, earliest_nights))
 
-    def location_frequencies(self, reject_flight_midpoints=True):
+    def location_frequencies(self,
+                             reject_flight_midpoints=True,
+                             start_date=None,
+                             end_date=None):
         """Returns a dictionary with keys of cities and values of
         dictionaries with latitudes, longitudes, and locations."""
         
@@ -39,11 +42,26 @@ class HotelDataFrame:
         for row in self.data.values.tolist():
             nights = row[1]
             city = row[2]
+            checkout = row[0].date()
+            fm = first_morning(checkout, nights)
             if reject_flight_midpoints:
                 location_list = city.split("/")
                 if (location_list[0] == "Overnight Flight"
                         and "-" in location_list[1]):
                     continue
+            if start_date:
+                if checkout < start_date:
+                    continue
+                elif fm < start_date:
+                    # Subtract number of nights before start date.
+                    nights = nights - (start_date - fm).days
+            if end_date:
+                if fm > end_date:
+                    continue
+                elif checkout > end_date:
+                    # Subtract number of nights after end date.
+                    nights = nights - (checkout - end_date).days
+                
             if city in frequencies:
                 frequencies[city]['frequency'] += nights
             else:
